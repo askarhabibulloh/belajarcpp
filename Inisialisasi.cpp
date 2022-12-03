40231952251f4f3fcfc543ac0e6dc0e02b3bab10
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-03 22:01:37 +0700

    belajarcpp

---
+++ Inisialisasi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"masukkan nilai : ";
+    cin>>a;
+    cout<<"nilai yang kamu masukkan adalah: ";
+    cout<<a<<endl;
+    cout<<"memori yang digunakan adalah : "<<sizeof(a)<<" byte";
+    return 0;
+}
---
+++ Variabel.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main(){
+    int a = 10 ; //ini deklarasi dan inisialisasi dalam satu baris
+    std::cout<< a <<std::endl;
+    std::cin.get();
+    int b; //ini deklarasi
+    b = 15; // ini inisialisasi
+    std::cout << b <<std::endl;
+    return 0;
+}
