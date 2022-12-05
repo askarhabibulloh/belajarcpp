ee852825f83f1ad14117915997230630c19b4332
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-05 15:39:09 +0700

    belajarcpp

--- komparasi.cpp
+++ komparasi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int a = 2;
     int b = 2;
-    bool hasil1,hasil2;
+    bool hasil1,hasil2,hasil3,hasil4,hasil5,hasil6;
 
     //==
     hasil1=(a==b);
@@ -12,7 +12,19 @@ int main(){
     //!=
     hasil2=(a!=b);
 
+    //kurang dari
+    hasil3=(a<b);
+
+    //lebih dari
+    hasil4 = (a>b);
+
+    //kurang dari sama dengan
+    hasil5 =(a<=b);
+
+    //lebih dari sama dengan
+    hasil6 =(a>=b);
+
     //display
-    cout<<hasil1<<endl<<hasil2<<endl;
+    cout<<hasil1<<endl<<hasil2<<endl<<hasil3<<endl<<hasil4<<endl<<hasil5<<endl<<hasil6<<endl;
 
 }
