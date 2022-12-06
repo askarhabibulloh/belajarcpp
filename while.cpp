446bb14c264bc9d5a22c79b86e57a562e6318bf5
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-06 10:17:42 +0700

    6 Desember 22

---
+++ while.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int b = 5;
+    while (b<15){
+        cout<<"ini adalah nilai b sekarang --> "<<b<<endl;
+        b++;
+    } 
+    cout<<endl<<endl;
+    int c =5;
+    while (c<15)
+    {
+        cout<<"ini adalah nilai c sekarang -->"<<c<<endl;
+        c += 2;
+    }
+    
+    
+    return 0;
+}
