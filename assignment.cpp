9370d170ca8ac3e4b1b699c322db1a3f2d0932f4
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-06 10:18:12 +0700

    6 Des 22

---
+++ assignment.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=10;
+    cout<<"nilai a sekarang adalah "<<a<<endl;
+
+
+    a += 3;
+    cout<<"nilai a sekarang adalah "<<a<<endl;
+
+    a -= 3;
+    cout<<"nilai a sekarang adalah "<<a<<endl;
+
+    a /= 3;
+    cout<<"nilai a sekarang adalah "<<a<<endl;
+
+    a *= 3;
+    cout<<"nilai a sekarang adalah "<<a<<endl;
+
+    a %= 3;
+    cout<<"nilai a sekarang adalah "<<a<<endl;
+    return 0;
+}
---
+++ incredecre.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //increment decrement
+    int a=5,b=5;
+
+    //pre increment
+    cout<<a<<endl;
+    cout<<++a<<endl;
+    cout<<a<<endl<<endl;
+
+
+    //post increment
+    cout<<b<<endl;
+    cout<<b++<<endl;
+    cout<<b<<endl<<endl;
+
+
+    //pre decrement
+    cout<<a<<endl;
+    cout<<--a<<endl;
+    cout<<a<<endl<<endl;
+
+
+    //post decrement
+    cout<<b<<endl;
+    cout<<b--<<endl;
+    cout<<b<<endl;
+
+
+    return 0;
+}
