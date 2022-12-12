9331a4f6796ce26938e6860929c28176464003d6
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-12 10:05:55 +0700

    12 Des 22

---
+++ samakaki.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cout<<"masukkan jumlah baris \n";
+    cin>>n;
+    cout<<endl;
+
+    cout<<"pola lima \n";
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = n;k > i; k--)
+        {
+            cout<<"  ";
+        }
+        
+        for (int j = 1; j <= 2*i-1; j++)
+        {
+        cout<<"* ";
+        }
+        cout<<endl;
+    }
+        cout<<endl<<endl;
+
+    
+    cout<<"pola enam \n";
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 1;k < i; k++)
+        {
+            cout<<"  ";
+        }
+        
+        for (int j = n; j >= 2*i-n; j--)
+        {
+        cout<<"* ";
+        }
+        cout<<endl;
+    }
+        cout<<endl<<endl;
+    ;
+    return 0;
+    return 0;
+}
