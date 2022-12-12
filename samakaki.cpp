c86123d083c6255774ad9aa7d1eaf3f3306716f2
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-12 10:08:39 +0700

    12 Des 22

--- samakaki.cpp
+++ samakaki.cpp
@@ -43,7 +43,35 @@ int main()
         cout<<endl;
     }
         cout<<endl<<endl;
-    ;
-    return 0;
+
+    cout<<"pola tujuh \n";
+
+    for (int i = 1; i <= n-1; i++)
+    {
+        for (int k = n;k > i; k--)
+        {
+            cout<<"  ";
+        }
+        
+        for (int j = 1; j <= 2*i-1; j++)
+        {
+        cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 1;k < i; k++)
+        {
+            cout<<"  ";
+        }
+        
+        for (int j = n; j >= 2*i-n; j--)
+        {
+        cout<<"* ";
+        }
+        cout<<endl;
+    }
+        cout<<endl<<endl;
     return 0;
 }
