7c0477f8e0ca1171798fb2266de29f4e4a37f70a
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2022-12-12 10:40:12 +0700

    12 Des 22

---
+++ 26_fungsi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    cout<<"Menghitung akar dari : ";
+    cin>>x;
+    double y = sqrt(x);
+    cout<<"akar dari "<<x<<" adalah "<<y<<endl;
+
+    return 0;
+}
